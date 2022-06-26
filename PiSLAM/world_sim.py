604d6df259c76f590a7a1346c6dc21a94cd1afdc
Nathan <nathanielheidt@gmail.com>
2022-06-26 12:18:24 -0700

    Seemingly working graphslam! Weights would probably improve everything

--- PiSLAM/world_sim.py
+++ PiSLAM/world_sim.py
@@ -95,7 +95,7 @@ class Landmark:
         return self.priority > other.priority
 
 class LandmarkDB:
-    def __init__(self, threshold=5, mincount=3):
+    def __init__(self, threshold=25, mincount=3):
         self.landmarks: list[Landmark] = []
         self.landmark_map: dict[int, Landmark] = {}
         self.threshold = threshold
@@ -118,21 +118,28 @@ class LandmarkDB:
         heapq.heappush(self.landmarks, landmark)
         self.cur_key += 1
         return None, None
+        
+    def update_position(self, key: int, position: np.array):
+        self.landmark_map[key].position = position
 
 class graphSLAM:
     def __init__(self):
         self.landmark_database = LandmarkDB()
         self.deltas = []
         self.position_landmarks = []
+        self.landmark_deltas = []
         self.matching_landmarks = []
+        self.last_estimated_position = np.array([0., 0.])
         self.num_observations = 0
         self.offset = np.array((0.0, 0.0))
         self.unique_landmarks: set = set([])
 
     def set_offset(self, offset):
+        self.last_estimated_position = np.array(offset)
         if len(self.deltas) > 0:
             self.deltas[0] = offset
         else:
+            self.landmark_deltas.append([])
             self.deltas.append(offset)
             self.position_landmarks.append([])
     
@@ -141,8 +148,13 @@ class graphSLAM:
         self.deltas.append(delta)
         # want to keep track of what landmarks this position is associated with
         self.position_landmarks.append([])
+        self.landmark_deltas.append([])
+        estimated_position = self.last_estimated_position + delta
+
         for feature in features:
-            key, position = self.landmark_database.find_matching_landmark(feature)
+            estimated_feature_location = feature + estimated_position
+            print("feature loc: ", estimated_feature_location)
+            key, position = self.landmark_database.find_matching_landmark(estimated_feature_location)
             # if there are existing landmarks for this position, add them to the list
             if key is not None:
                 print("got landmark match!")
@@ -150,6 +162,7 @@ class graphSLAM:
                 self.matching_landmarks.append(key)
                 print("LEngths: ", len(self.deltas), len(self.position_landmarks))
                 self.position_landmarks[len(self.deltas) - 1].append(key)
+                self.landmark_deltas[len(self.deltas) - 1].append(feature)
                 self.unique_landmarks.add(key)
         # Current row form
         # X0 X1 X2 ... Y0 Y1 Y2 ... LX0 LY0 LX1 LY2 ...
@@ -169,22 +182,22 @@ class graphSLAM:
         # TODO(HEIDT) transfer to a sparse matrix
         # generate the matrix and vector, multiply sizes by two as this is a 2D problem
         matsize = len(self.deltas)*2 + len(self.unique_landmarks)*2
-        observation_size = self.num_observations*2 + matsize
+        observation_size = self.num_observations*2 + len(self.deltas)*2
         mat = np.zeros((observation_size, matsize))
         vec = np.zeros(observation_size)
 
         # fill out relative constraints for X
         mat[0,0] = 1
         for i in range(len(self.deltas[:-1])):
-            mat[i+1, i] = 1
-            mat[i+1, i+1] = -1
+            mat[i+1, i] = -1
+            mat[i+1, i+1] = 1
 
         # fill out relative constraints for Y
         plen = len(self.deltas)
         mat[plen, plen] = 1
         for i, position in enumerate(self.deltas[:-1]):
-            mat[i + 1 + plen, i + plen] = 1
-            mat[i + 1 + plen, i + 1 + plen] = -1
+            mat[i + 1 + plen, i + plen] = -1
+            mat[i + 1 + plen, i + 1 + plen] = 1
 
         # fill out vector values
         #print("positions: ", self.positions)
@@ -207,29 +220,29 @@ class graphSLAM:
             col += 2
 
         # fill out relative constraints for observations
-        print("mat: ")
-        print(mat)
-        print("vec: ")
-        print(vec)
-        print("shapes")
-        print(mat.shape, vec.shape)
         
         row = len(self.deltas)*2
-        for i, landmark_list in enumerate(self.position_landmarks):
+        for j, landmark_list in enumerate(self.position_landmarks):
             if len(landmark_list) > 0:
-                for landmark_id in landmark_list:
+                #print("landmark lengths: ", len(landmark_list), len(self.landmark_deltas))
+                for i, landmark_id in enumerate(landmark_list):
                     # fill in for the x coordinate
-                    print("row: ", row, landmark_keys[landmark_id])
-                    mat[row, i] = 1
-                    mat[row, landmark_keys[landmark_id]] = -1
-                    vec[row] = self.landmark_database.landmark_map[landmark_id].position[0]
+                    mat[row, j] = -1
+                    mat[row, landmark_keys[landmark_id]] = 1
+                    vec[row] = self.landmark_deltas[j][i][0]
                     row += 1
                     # Do the same for the Y coordinate
-                    mat[row, i + plen] = 1
-                    mat[row, landmark_keys[landmark_id]+1] = -1
-                    vec[row] = self.landmark_database.landmark_map[landmark_id].position[1]
+                    mat[row, j + plen] = -1
+                    mat[row, landmark_keys[landmark_id]+1] = 1
+                    vec[row] = self.landmark_deltas[j][i][1]
                     row += 1
 
+        #print("mat: ")
+        #print(mat)
+        #print("vec: ")
+        #print(vec)
+        #print("shapes")
+        #print(mat.shape, vec.shape)
 
         res = np.linalg.lstsq(mat, vec)
         print("res: ", res[0])
@@ -237,7 +250,13 @@ class graphSLAM:
         for i in range(len(self.deltas)):
             new_pos = np.array((res[0][i], res[0][i+len(self.deltas)]))
             new_positions.append(new_pos)
-        return new_positions
+
+        for key, col in landmark_keys.items():
+            position = np.array([res[0][col], res[0][col+1]])
+            self.landmark_database.update_position(key, position)
+
+        self.last_estimated_position = new_positions[-1]
+        return np.array(new_positions)
 
 
 world = World("floorplan.png")
@@ -253,23 +272,28 @@ slam = graphSLAM()
 true_positions = []
 updated_positions = []
 estimated_positions = []
+dedrec_positions = []
 
 @window.event
 def on_draw():
     window.clear()
     image.blit(0, 0)
-    rays = world.cast_ideal_rays(mouse_pos, 100)
+    rays = world.cast_ideal_rays(mouse_pos, 360)
     raygrid = world.fill_point_grid(rays)
     spikes = world.find_spikes(rays)
     world.draw_spikes(raygrid, spikes)
     for i, position in enumerate(true_positions):
         est = (int(estimated_positions[i][0]), int(estimated_positions[i][1]))
+        ded = (int(dedrec_positions[i][0]), int(dedrec_positions[i][1]))
         cv2.circle(raygrid, position, 4, (0, 255, 0), 2)
-        cv2.circle(raygrid, est, 4, (0, 0, 255), 2)
+        cv2.circle(raygrid, est, 4, (255, 0, 0), 2)
+        cv2.circle(raygrid, ded, 4, (0, 0, 255), 2)
         if i < len(true_positions) - 1:
             est2 = (int(estimated_positions[i+1][0]), int(estimated_positions[i+1][1]))
+            ded2 = (int(dedrec_positions[i+1][0]), int(dedrec_positions[i+1][1]))
             cv2.line(raygrid, position, true_positions[i+1], (0, 255, 0), 1)
-            cv2.line(raygrid, est, est2, (0, 0, 255), 1)
+            cv2.line(raygrid, est, est2, (255, 0, 0), 1)
+            cv2.line(raygrid, ded, ded2, (0, 0, 255), 1)
 
     cv2.imshow('frame', raygrid)
     cv2.waitKey(1)
@@ -286,25 +310,33 @@ def on_mouse_motion(x, y, dx, dy):
 
 @window.event
 def on_mouse_press(x, y, button, modifiers):
-    global true_positions, estimated_positions
+    global true_positions, estimated_positions, dedrec_positions
     true_positions.append((x, y))
+    inverted = (x, world.world_grid.shape[0] - y)
+    position = np.array(inverted, dtype=np.float)
 
     if len(true_positions) == 1:
         estimated_positions = [np.array((x, y))]
-        slam.set_offset(np.array((x, y)))
+        dedrec_positions.append(np.array((x, y)))
+        slam.set_offset(np.array(inverted))
     else:
-        rays = world.cast_ideal_rays((x, y), 100)
-        spikes = world.find_spikes(rays)
-     
-        position = np.array((float(x), float(y)))
-        last_position = np.array(true_positions[-2])
-        delta = last_position - position
-        print("delta: ", delta)
-        delta += np.random.rand(2)*5.0
+        rays = np.array(world.cast_ideal_rays((x, y), 100))
+        rays[:, 1] = world.world_grid.shape[0] - rays[:, 1]
+        spikes = np.array(world.find_spikes(rays))
 
-        relative_spikes = rays - position
+        last_position = np.array(true_positions[-2])
+        last_position[1] = world.world_grid.shape[0] - last_position[1]
+
+        delta = position - last_position
+        delta += np.random.rand(2)*5.0
+        dedrec_positions.append(dedrec_positions[-1] + np.array((delta[0], -delta[1])))
+
+        relative_spikes = spikes - position
         
         estimated_positions = slam.add_position(delta, relative_spikes)
+        estimated_positions[:,1] = world.world_grid.shape[0] - estimated_positions[:,1]
+        print("final: ", estimated_positions, true_positions)
+
     
 
 pyglet.app.run()
